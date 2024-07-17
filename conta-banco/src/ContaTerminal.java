c65acbcb3ce23cbdf533e42fd5b0834080484c6d
wberaldo92 <wellington.beraldo92@gmail.com>
2024-07-16 21:02:49 -0300

    Criado lógica da conta terminal utilizando o scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TO DO: Conhecer e importar a classe Scanner
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
 
         //Exibir as mensagewns para o nosso usuário
-
         //Obter pela scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da sua Agência!");
+        agencia = sc.next();
+        System.out.println("Por favor, digite o número da sua Conta!");
+        numero = sc.nextInt();
+        sc.nextLine(); //Consome a nova linha pendente
+        System.out.println("Por favor, digite o seu nome completo!");
+        nomeCliente = sc.nextLine();
+        System.out.println("Por favor, digite o seu saldo!");
+        saldo = sc.nextDouble();
+
+        //Fechar o Scanner
+        sc.close();
 
         //Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + "e seu saldo " + saldo + " já está disponível para saque!");
     }
 }
